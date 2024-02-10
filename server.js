ff36b36de1ac3eeda770ad6142465ff5e0fc729f
Jop <jopmolenaar@icloud.com>
2024-02-10 16:05:06 +0100

    added markdown to ejs template

--- server.js
+++ server.js
@@ -6,24 +6,26 @@ const path = require("path");
 let ejs = require("ejs");
 app.set("view engine", "ejs");
 app.use(express.static("static"));
-// Read the Markdown file
 
 app.get("/home", function (req, res) {
-    fs.readFile("process.md", "utf8", (err, data) => {
+    // Read the Markdown file
+    fs.readFile("static/process.md", "utf8", (err, data) => {
         if (err) {
             console.error("Error reading file:", err);
-            return;
+            return res.status(500).send("Error reading file");
         }
 
         // Parse Markdown to HTML
         const html = marked(data);
 
-        // Do something with the HTML content
-        console.log(html);
+        // Render the EJS template with the HTML content
+        res.render("index", { html: html });
     });
-    res.render("index");
-    // use res.render to load up an ejs view file
 });
 
-app.listen(3030);
-console.log("Server is listening on port 3030");
+app.listen(3030, () => {
+    console.log("Server is listening on port 3030");
+});
+
+// source chat gpt
+// promt: I want to get the content from a markdown file and parse it in to html
--- static/process.md
+++ static/process.md
@@ -2,4 +2,20 @@
 
 ## HI
 
+### h3
+
+#### h4
+
 jdnksjndfkj
+
+Hallo
+
+-   hi
+-   hi
+-   hi
+
+```js
+const headerImg = document.querySelector(
+    "main > div > section:nth-child(2) > img:nth-child(4)"
+);
+```
