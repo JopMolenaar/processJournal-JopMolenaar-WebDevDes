00e74777dd9a41379f134b84a5e9d1c27929b9fa
Jop <jopmolenaar@icloud.com>
2024-03-02 16:31:59 +0100

    most headings clickable

--- static/js/script.js
+++ static/js/script.js
@@ -3,23 +3,26 @@ const nav = document.querySelector("main nav ul");
 
 const makeNav = () => {
     const firstElement = document.querySelector("div h2");
-    firstElement.id = firstElement.textContent;
+    firstElement.id = firstElement.textContent.replace(" ", "-");
+    const text = firstElement.textContent.replace(" ", "-");
     output +=
-        `<li data-heading="h2" ><a href=#${firstElement.textContent}>` +
+        `<li data-heading="h2" ><a href=#${text}>` +
         firstElement.textContent +
         "</a></li>";
     let nextElement = firstElement.nextElementSibling;
     while (nextElement) {
         if (nextElement.tagName === "H2") {
-            nextElement.id = nextElement.textContent;
+            const text = nextElement.textContent.replace(" ", "-");
+            nextElement.id = text;
             output +=
-                `<li data-heading="h2" ><a href=#${nextElement.textContent}>` +
+                `<li data-heading="h2" ><a href=#${text}>` +
                 nextElement.textContent +
                 "</a></li>";
         } else if (nextElement.tagName === "H3") {
-            nextElement.id = nextElement.textContent;
+            const text = nextElement.textContent.replace(" ", "-");
+            nextElement.id = text;
             output +=
-                `<li data-heading="h3" ><a href=#${nextElement.textContent}>` +
+                `<li data-heading="h3" ><a href=#${text}>` +
                 nextElement.textContent +
                 "</a></li>";
         }
